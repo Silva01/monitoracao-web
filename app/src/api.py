fb827d186d43507c6a1eb346cc284ed702bdedec
root <root@colocar-pa-dentro.us-central1-c.c.generated-tine-168101.internal>
2018-12-21 00:58:21 +0000

    v0.0.17 - flask 2

--- app/src/api.py
+++ app/src/api.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def api():
+    complemento=os.environ['COMPLEMENTO']
+    if complemento == "Teste":
+        complemento = ""
+
+
     return app.response_class(
-        response="Eu Amo o Madeira",
+        response="Eu Amo o Madeira " + complemento,
         status=200,
         mimetype='application/json'
     )
